d8680a40a1bb00e0fcae68fa16372421a888e54e
guihun <615815574@qq.com>
2019-03-23 12:49:20 +0800

    leetcode

---
+++ 141.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None:
+            return False
+        if head.next is None:
+            return False
+        fast=head.next
+        slow=head
+        while fast!=slow:
+            if not fast or not fast.next:
+			    return False
+            fast=fast.next.next
+            slow=slow.next
+        return True
